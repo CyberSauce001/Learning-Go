91a6a44aa5f3f570e4aa380b88832cbd188af0b4
Ethereal15 <xionnguyen@gmail.com>
2018-11-29 13:00:13 -0800

    Create variable.go

---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+const Pi = 3.14
+const World = "世界"
+
+func main() {
+	var i, j int = 15, 99
+	k := 5
+	c, python, java := false, false, "hello world!"
+
+	fmt.Println(i, j, k, c, python, java)
+	
+
+	
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+
+}
